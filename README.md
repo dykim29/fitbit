4955888d42deb653ec38d9cd8ea7083030e63443
DoYoung <doyoung_kim@hotmail.com>
2021-07-14 19:42:13 +0100

    [feat]Add client.py for Oauth authorisation and notebook for analysis

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Fitbit data
 
-This repo downloads my Fitbit data and processes it, for analysis.
+This repo downloads Fitbit data and processes it, for analysis.
 
+## Setting up secret config
+In order to use this repo, you need to add a file called `secret_config.json` in the main directory with this format:
+```
+{
+  "client_id": CLIENT_ID,
+  "client_secret": CLIENT_SECRET,
+  "redirect_url": REDIRECT_URL,
+  "auth_url": AUTH_URL, 
+  "access_token_url": ACCESS_TOKEN_URL
+}
+```
+The values can be obtained through the Fitbit API dev portal, by registering an app to obtain API client credentials
---
+++ client.py
@@ -0,0 +1,21 @@
+import json
+
+import fitbit
+
+import gather_keys_oauth2 as Oauth2
+
+
+def get_client():
+    with open('secret_config.json') as json_file:
+        data = json.load(json_file)
+    CLIENT_ID = data['client_id']
+    CLIENT_SECRET = data['client_secret']
+    REDIRECT_URL = data['redirect_url']
+    server = Oauth2.OAuth2Server(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
+    server.browser_authorize()
+    ACCESS_TOKEN = str(server.fitbit.client.session.token['access_token'])
+    REFRESH_TOKEN = str(server.fitbit.client.session.token['refresh_token'])
+    auth2_client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET, oauth2=True, access_token=ACCESS_TOKEN,
+                                 refresh_token=REFRESH_TOKEN)
+    return auth2_client
+
